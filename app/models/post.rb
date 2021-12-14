884380c7801819b7ab72f3c223a4eafda3200e35
Landon Skinner <landonjskinner@gmail.com>
2021-12-14 08:40:53 -0600

    commit tree 0e002daefe2a6622ff0a12b0eecf0959262a4dd1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :title
+
+    def must_be_clickbait
+        unless title.include?(%w("Won't Believe" "Secret" "Top" "Guess"))
+            errors.add(:title, "not clickbaity")
+        end
+    
+    end
+
 end
